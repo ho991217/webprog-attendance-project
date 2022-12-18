99ab82374448a2a3c03ca73570228c2a3146c4d3
이호연 <ho991217@kakao.com>
2022-12-19 01:17:52 +0900

    complete

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -40,7 +40,7 @@ const InnerContainer = styled.div`
 
   @media (max-width: 600px) {
     grid-template-columns: 1fr;
-    grid-template-rows: 1fr 3fr 3fr 2fr 4fr;
+    grid-template-rows: 1fr 3fr 3fr 4fr;
     grid-gap: 15px;
     padding: 15px;
     grid-template-areas:
@@ -121,7 +121,7 @@ const Header = styled.header`
 
 const H1 = styled.h1`
   @media (max-width: 600px) {
-    font-size: 2rem;
+    font-size: 1.75rem;
   }
   @media (min-width: 601px) {
     font-size: 2rem;
@@ -137,21 +137,25 @@ const H1 = styled.h1`
 const LevelBar = styled.div<{ level?: string }>`
   display: flex;
   align-items: center;
-  font-size: 0.7rem;
+  font-size: 0.2rem;
   font-weight: 500;
   color: rgba(0, 0, 0, 0.7);
   margin-top: 10px;
+  background-color: rgba(0, 0, 0, 0.15);
+  padding: 4px 10px;
+  border-radius: 9999px;
   div {
     margin: 0 10px;
     border-radius: 9999px;
     @media (max-width: 600px) {
       width: 100px;
-      height: 10px;
+      /* height: 10px; */
     }
     @media (min-width: 601px) {
       width: 200px;
-      height: 10px;
+      /* height: 10px; */
     }
+    height: calc(100% + 8px);
     background: ${({ level, theme }) => `linear-gradient(
       90deg,
       ${theme.colors.gradient[level || "정보 없음"]}
@@ -192,6 +196,145 @@ const WeatherInfoCell = styled(Cell)`
   height: 100%;
 `;
 
+const WeatherWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-weight: 400;
+
+  span {
+    color: #ed7547;
+    margin-right: 5px;
+    padding-top: 3px;
+  }
+`;
+
+const HiLoWrapper = styled(WeatherWrapper)``;
+
+const CurrentTempWrapper = styled(WeatherWrapper)``;
+
+const SensibleTempWrapper = styled(WeatherWrapper)``;
+
+const PcpWrapper = styled(WeatherWrapper)``;
+
+const PcpMsgWrapper = styled(WeatherWrapper)``;
+
+const TrafficInfoCell = styled(Cell)`
+  grid-area: trafficInfo;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: scroll;
+  @media (max-width: 600px) {
+    padding-top: 25px;
+    h2 {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+const TrafficWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+    margin: 25px 0;
+    justify-content: flex-start;
+  }
+`;
+
+const AvgTrafficCircle = styled.div<{ level?: string }>`
+  width: 100px;
+  height: 100px;
+  @media (max-width: 600px) {
+    width: 80px;
+    height: 80px;
+  }
+  @media (min-width: 601px) {
+    margin-right: 10px;
+  }
+  font-weight: 500;
+  color: #eee;
+  border-radius: 50%;
+  background-color: ${({ level }) =>
+    level === "원활"
+      ? "rgba(0,191,165,1)"
+      : level === "서행"
+      ? "rgba(255,165,0,1)"
+      : level === "정체"
+      ? "rgba(255,0,0,1)"
+      : "rgba(0,0,0,0.1)"};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  font-size: 1.8rem;
+  span {
+    font-size: 0.75rem;
+    margin-bottom: 2px;
+  }
+`;
+
+const TrafficChartWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  @media (max-width: 600px) {
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 25px 0;
+  }
+`;
+
+const RoadName = styled.div`
+  font-weight: 500;
+  font-size: 1.1rem;
+  word-break: keep-all;
+`;
+
+const RoadCell = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media (max-width: 600px) {
+    margin: 5px 0;
+  }
+  @media (min-width: 601px) {
+    flex-direction: column;
+    margin: 0 10px;
+  }
+`;
+
+const RoadSpeed = styled.div`
+  font-weight: 400;
+  font-size: 0.8rem;
+  @media (max-width: 600px) {
+    margin: 0 5px;
+  }
+  @media (min-width: 601px) {
+    margin: 2.5px 0 7px 0;
+  }
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const RoadLevel = styled.div<{ level?: string }>`
+  font-weight: 400;
+  font-size: 0.95rem;
+  padding: 4px 12px 5px 12px;
+  border-radius: 9999px;
+  background-color: ${({ level }) =>
+    level === "원활"
+      ? "rgba(0,191,165,1)"
+      : level === "서행"
+      ? "rgba(255,165,0,1)"
+      : level === "정체"
+      ? "rgba(255,0,0,1)"
+      : "rgba(0,0,0,0.1)"};
+  color: #eee;
+`;
+
 const NewsCell = styled(Cell)`
   grid-area: news;
   flex-direction: column;
@@ -200,14 +343,6 @@ const NewsCell = styled(Cell)`
   padding: 0 20px;
   height: 100%;
 `;
-
-const TrafficInfoCell = styled(Cell)`
-  grid-area: trafficInfo;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
 const Table = styled.table``;
 const TableHead = styled.thead``;
 const TableHeadCell = styled.th`
@@ -336,11 +471,59 @@ const Detail = () => {
             )}
           </NewsCell>
           <WeatherInfoCell>
-            {thisInfo?.weather.pcp_type}
-            {thisInfo?.weather.pcp_msg}
+            <H2>날씨</H2>
+            {thisInfo ? (
+              <>
+                <HiLoWrapper>
+                  <span className="material-icons">schedule</span>
+                  {"최저 " +
+                    thisInfo?.weather.min_temp +
+                    " ℃ ~ 최고 " +
+                    thisInfo?.weather.max_temp +
+                    " ℃"}
+                </HiLoWrapper>
+                <CurrentTempWrapper>
+                  <span className="material-icons">thermostat</span>[ 현재 기온
+                  ] {thisInfo?.weather.temp} ℃
+                </CurrentTempWrapper>
+                <SensibleTempWrapper>
+                  <span className="material-icons">air</span>[ 체감 기온 ]{" "}
+                  {thisInfo?.weather.sensibe_temp} ℃
+                </SensibleTempWrapper>
+                <PcpWrapper>
+                  <span className="material-icons">cloud</span>
+                  {thisInfo?.weather.pcp + " " + thisInfo?.weather.pcp_type}
+                </PcpWrapper>
+                <PcpMsgWrapper>
+                  <span className="material-icons">umbrella</span>
+                  {thisInfo?.weather.pcp_msg}
+                </PcpMsgWrapper>
+              </>
+            ) : (
+              <Spinner />
+            )}
           </WeatherInfoCell>
           <TrafficInfoCell>
-            {thisInfo?.traffic_status.avg.level}
+            <H2>정체 구간 정보 (상위 10개 정체 구간)</H2>
+            <TrafficWrapper>
+              <AvgTrafficCircle level={thisInfo?.traffic_status.avg.level}>
+                <span>구간 평균</span>
+                {thisInfo?.traffic_status.avg.level}
+              </AvgTrafficCircle>
+              <TrafficChartWrapper>
+                {thisInfo?.traffic_status.roads
+                  .slice(0)
+                  .sort((a, b) => Number(a.speed) - Number(b.speed))
+                  .slice(0, 10)
+                  .map((road) => (
+                    <RoadCell>
+                      <RoadName>{road.name}</RoadName>
+                      <RoadSpeed>{road.speed} km/h</RoadSpeed>
+                      <RoadLevel level={road.level}>{road.level}</RoadLevel>
+                    </RoadCell>
+                  ))}
+              </TrafficChartWrapper>
+            </TrafficWrapper>
           </TrafficInfoCell>
         </InnerContainer>
       </Container>
